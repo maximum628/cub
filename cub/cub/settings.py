037dbe0a8047e164460824683fcf363a37021cd6
maximum628 <maximum95628@gmail.com>
2015-11-18 19:39:21 +0200

    Add tastypie and postresql backend

--- cub/cub/settings.py
+++ cub/cub/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'tastypie',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,11 +78,14 @@ WSGI_APPLICATION = 'cub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cub',
+        'USER': 'cub',
+        'PASSWORD': 'cuby',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
