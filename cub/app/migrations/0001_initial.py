dfeeb171f8b78aacbeb17dbf8fc378e06f50065c
maximum628 <maximum95628@gmail.com>
2015-11-21 08:41:19 +0200

    Add Account model and helper methods

--- cub/app/migrations/0001_initial.py
+++ cub/app/migrations/0001_initial.py
@@ -14,9 +14,16 @@ class Migration(migrations.Migration):
             name='Account',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('github_url', models.URLField()),
+                ('username', models.CharField(unique=True, max_length=50)),
                 ('github_token', models.TextField()),
+                ('name', models.CharField(max_length=50)),
+                ('email', models.CharField(max_length=70)),
+                ('github_url', models.URLField()),
                 ('avatar_url', models.TextField()),
             ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='account',
+            unique_together=set([('username', 'email')]),
         ),
     ]
--- cub/app/models.py
+++ cub/app/models.py
@@ -1,10 +1,46 @@
+from django.contrib.auth.hashers import make_password, check_password
 from django.db import models
 
 class Account(models.Model):
 
     class Meta:
         app_label = 'app'
+        unique_together = (('username', 'email'),)
 
-    github_url = models.URLField()
-    github_token = models.TextField()
+    username = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    github_token = models.TextField(blank=False, null=False)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, blank=False, null=False)
+    github_url = models.URLField(blank=False, null=False)
     avatar_url = models.TextField()
+
+    @classmethod
+    def save(cls, data):
+        if Account.objects.filter(username=data['username'], email=data['email']):
+            account = Account.objects.get(username=data['username'], email=data['email'])
+
+            if not check_password(data['github_token'], account.github_token):
+                account.github_token = make_password(data['github_token'])
+                account.save()
+            return account
+        else:
+            return cls.create(data)
+
+    @classmethod
+    def create(cls, data):
+        account = Account(
+            username=data['username'], name=data['name'],
+            email=data['email'], github_url=data['github_url'],
+            avatar_url=data['avatar_url'])
+
+        account.github_token = make_password(data['github_token'])
+        account.save()
+        return account
+
+    @classmethod
+    def authorize(cls, username, password):
+        if Account.objects.filter(username=username):
+            account = Account.objects.get(username=username)
+            if check_password(password, account.github_token):
+                return account
+        return None
