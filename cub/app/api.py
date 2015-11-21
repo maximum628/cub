e66c05374b7d32b9f52fcaf08acada9095a64b0c
maximum628 <maximum95628@gmail.com>
2015-11-21 08:58:58 +0200

    Update api fields

--- cub/app/api.py
+++ cub/app/api.py
@@ -5,4 +5,5 @@ from app.models import Account
 class AccountResource(ModelResource):
     class Meta:
         queryset = Account.objects.all()
+        excludes = ['github_token', 'id']
         allowed_methods = ['get']
