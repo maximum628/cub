f332d6e2cf11645b318649b49133a54c41154ba0
maximum628 <maximum95628@gmail.com>
2015-11-22 10:50:22 +0200

    Add Mongo models for Account, contributions

--- cub/app/models.py
+++ cub/app/models.py
@@ -1,21 +1,21 @@
 from django.contrib.auth.hashers import make_password, check_password
-from django.db import models
+import mongoengine
 
-class Account(models.Model):
+class Account(mongoengine.Document):
 
-    class Meta:
-        app_label = 'app'
-        unique_together = (('username', 'email'),)
+    meta = {
+        'indexes': [{'fields': ['username', 'email']}]
+    }
 
-    username = models.CharField(max_length=50, unique=True, blank=False, null=False)
-    github_token = models.TextField(blank=False, null=False)
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=70, blank=False, null=False)
-    github_url = models.URLField(blank=False, null=False)
-    avatar_url = models.TextField()
+    username = mongoengine.StringField(required=True)
+    github_token = mongoengine.StringField(required=True)
+    name = mongoengine.StringField()
+    email = mongoengine.EmailField(required=True)
+    github_url = mongoengine.URLField(required=True)
+    avatar_url = mongoengine.URLField()
 
     @classmethod
-    def save(cls, data):
+    def check(cls, data):
         if Account.objects.filter(username=data['username'], email=data['email']):
             account = Account.objects.get(username=data['username'], email=data['email'])
 
@@ -44,3 +44,29 @@ class Account(models.Model):
             if check_password(password, account.github_token):
                 return account
         return None
+
+
+class Contribution(mongoengine.Document):
+    meta = {
+        'abstract': True,
+        'indexes': [{'fields': ['account_id', 'created_date']}],
+        'ordering': ['-created_date']
+    }
+
+    account_id = mongoengine.IntField(required=True)
+    html_url = mongoengine.URLField(required=True)
+    url = mongoengine.URLField(required=True)
+    html_repo_url = mongoengine.URLField(required=True)
+    repo_url = mongoengine.URLField(required=True)
+    created_date = mongoengine.DateTimeField(required=True)
+    stats = mongoengine.DictField()
+
+
+class CommitContribution(Contribution):
+
+    files_stats = mongoengine.DictField()
+
+
+class PRContribution(Contribution):
+
+    state = mongoengine.StringField(required=True)
