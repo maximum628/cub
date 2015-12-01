361e8e4f8cc7f5ad5123a042a7e0c46b506d7e71
maximum628 <maximum95628@gmail.com>
2015-12-01 19:40:48 +0200

    Add authentication and authorization to api

--- cub/app/api.py
+++ cub/app/api.py
@@ -1,3 +1,5 @@
+from tastypie.authentication import SessionAuthentication
+from tastypie.authorization import DjangoAuthorization
 from tastypie.resources import ModelResource
 from tastypie_mongoengine import resources
 
@@ -8,23 +10,29 @@ from app.models import (Account, CommitContribution, PRContribution, Repository,
 class AccountResource(ModelResource):
     class Meta:
         queryset = Account.objects.all()
-        excludes = ['github_token', 'id']
+        excludes = ['id', 'github_token', 'password', 'first_name', 'last_name',
+                    'is_active', 'is_staff', 'is_superuser']
         allowed_methods = ['get']
+        authentication = SessionAuthentication()
+        authorization = DjangoAuthorization()
 
 
 class CommitContributionResource(resources.MongoEngineResource):
     class Meta:
         queryset = CommitContribution.objects.all()
         allowed_methods = ['get']
+        authentication = SessionAuthentication()
 
 
 class PRContributionResource(resources.MongoEngineResource):
     class Meta:
         queryset = PRContribution.objects.all()
         allowed_methods = ['get']
+        authentication = SessionAuthentication()
 
 
 class RepositoryResource(resources.MongoEngineResource):
     class Meta:
         queryset = Repository.objects.all()
         allowed_methods = ['get']
+        authentication = SessionAuthentication()
