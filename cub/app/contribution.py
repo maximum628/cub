15ae6cb2aeb3c6fbb0f291d8ef55f6b6828a39ed
maximum628 <maximum95628@gmail.com>
2015-12-03 22:35:22 +0200

    Add get_pulls method

--- cub/app/contribution.py
+++ cub/app/contribution.py
@@ -1,11 +1,23 @@
 import simplejson
 from github import Github
 
-from app.models import Repository
+from app.models import Repository, PRContribution
 
 def get_repos(account):
-        """Save account public repositories data. """
-        connection = Github(login_or_token=account.github_token)
-        repos = connection.get_user().get_repos(type='public')
-        for repo in repos:
-            Repository.create_or_update(repo.raw_data, account)
+    """Save account public repositories data. """
+    connection = Github(login_or_token=account.github_token)
+    repos = connection.get_user().get_repos(type='public')
+    for repo in repos:
+        Repository.create_or_update(repo.raw_data, account)
+
+
+def get_pulls(account):
+    """Save account merged pull requests. """
+    connection = Github(login_or_token=account.github_token, api_preview=False)
+    # Set query to find contributions
+    qualifiers = {'is': 'merged', 'type': 'pr', 'author': account.username}
+    pulls = connection.search_issues(
+        query='', sort='updated', order='desc', **qualifiers)
+
+    for pull in pulls:
+        PRContribution.create_or_update(pull.raw_data, account)
