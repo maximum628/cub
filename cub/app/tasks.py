b2635ac4b1a60b8dc5be265ae93adb814a1ae294
maximum628 <maximum95628@gmail.com>
2015-12-06 15:05:52 +0200

    Remove background decorator

--- cub/app/tasks.py
+++ cub/app/tasks.py
@@ -1,6 +1,5 @@
 from app.contribution import get_repos, get_pulls, get_live_score
 
-@background(schedule=10)
 def get_contributions(account_github_token, account_username):
     get_repos(account_github_token, account_username)
     get_pulls(account_github_token, account_username)
